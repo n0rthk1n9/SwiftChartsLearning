8fcd5f51aa6dcc437246d49e1e831a0bb91f873c
Jan Armbrust <jan.armbrust@googlemail.com>
2024-11-15 12:18:56 +0100

    Project setup

--- SwiftChartsLearning/SwiftChartsLearningApp.swift
+++ SwiftChartsLearning/SwiftChartsLearningApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftChartsLearningApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
--- SwiftChartsLearning/Views/MainView.swift
+++ SwiftChartsLearning/Views/MainView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainView.swift
 //  SwiftChartsLearning
 //
 //  Created by Jan Armbrust on 15/11/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    MainView()
 }
