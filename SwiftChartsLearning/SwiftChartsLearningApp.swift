d324aab2f9ea9c0696d90ec3c0c2c1658c56b463
Jan Armbrust <jan.armbrust@googlemail.com>
2024-11-15 12:15:40 +0100

    Initial Commit

---
+++ SwiftChartsLearning/SwiftChartsLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsLearningApp.swift
+//  SwiftChartsLearning
+//
+//  Created by Jan Armbrust on 15/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
